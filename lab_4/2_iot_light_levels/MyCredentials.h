b630b9f33078000a8299740bbd3ea27de9f78122
Alex Shenfield <alex.shenfield@gmail.com>
2025-02-04 15:20:48 +0000

    added lab 4 code and lab sheet

---
+++ lab_4/2_iot_light_levels/MyCredentials.h
@@ -0,0 +1,8 @@
+// my wifi credentials
+const char my_ssid[] = "<you ssid>";
+const char my_pass[] = "<your wifi password>";
+
+// my adafruit io credentials
+#define CLIENT_ID "<an arbitrary client id>"
+#define USER_ID   "<your adafruit io username>"
+#define API_KEY   "<your adafruit io api key>"
